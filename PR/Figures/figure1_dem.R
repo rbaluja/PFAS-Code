dd4a7f7d3ca19b5be133ec17717ce60160ff940e
Robert Baluja <104729683+rbaluja@users.noreply.github.com>
2024-05-04 18:20:55 -0700

    updated figure one dem

--- PR/Figures/figure1_dem.R
+++ PR/Figures/figure1_dem.R
@@ -22,9 +22,8 @@ load(modify_path("Data_Verify/GIS/f1_watershed.RData"))
 f1_ws = f1_ws %>% 
   st_transform(4326) 
 f1_ws$site = c("C", "omega1", "omega2", "omega3")
-f1_ws = f1_ws[c(1, 4, 3, 2), ]
 
-f1_ws = f1_ws %>% purrr::map_df(rev) %>% st_as_sf()
+f1_ws = f1_ws %>% st_as_sf()
 
 f1 = nh_map_plot + 
   geom_raster(data = dem, aes(x = x, y = y, fill = elev)) +
@@ -37,13 +36,13 @@ f1 = nh_map_plot +
                        labels = c("0", "500m", "1000m", "1500m")) +
   theme(legend.position = "bottom") +
   ylim(44.18, 44.38) + xlim(-71.5, -71.15) +
-  annotate("point", x = -71.35, y = 44.27, color = "black", size = 6) +
+  annotate("point", x = -71.35, y = 44.27, color = "black", size = 12) +
   annotate("text", x = -71.35, y = 44.28, label = "C", vjust = 1.3, color = "black", size = 10, family = "arial") +
-  annotate("point", x = -71.25, y = 44.3, color = "grey77", size = 6) +
-  annotate("text", x = -71.25, y = 44.3, label = "ω[3]", parse = TRUE, hjust = 1., vjust = -0.5, color = "grey77", size = 10, family = "arial") +
-  annotate("point", x = -71.4, y = 44.26, color = "white", size = 6) +
+  annotate("point", x = -71.232, y = 44.3, color = "grey77", size = 8) +
+  annotate("text", x = -71.232, y = 44.3, label = "ω[3]", parse = TRUE, hjust = -.25, vjust = -0.45, color = "grey77", size = 10, family = "arial") +
+  annotate("point", x = -71.4, y = 44.26, color = "white", size = 8) +
   annotate("text", x = -71.4, y = 44.26, label = "ω[1]", parse = TRUE, vjust = -.5, hjust = 1, color = "white", size = 10)  + 
-  annotate("point", x = -71.33, y = 44.26, color = "firebrick", size = 6) +
+  annotate("point", x = -71.33, y = 44.26, color = "firebrick", size = 8) +
   annotate("text", x = -71.33, y = 44.26, label = "ω[2]", parse = TRUE, vjust = -0.3, hjust = -.3, color = "firebrick4", size = 10) + 
   # geom_segment(aes(x = -71.3, y = 44.27, xend = -71.26, yend = 44.25),
   #              arrow = grid::arrow(type = "closed", length = unit(0.2, "inches")),
@@ -52,20 +51,20 @@ f1 = nh_map_plot +
   #              arrow = grid::arrow(type = "closed", length = unit(0.2, "inches")),
   #              color = "white", size = 1, lineend = "round") +
   theme_void() +
-  theme(legend.position = "bottom",  # Add legend below the plot
-        legend.key = element_blank(),  # Optional: remove the legend key background
-        legend.title = element_blank(),  # Optional: style the legend title
+  theme(legend.position = "bottom",  
+        legend.key = element_blank(),  
+        legend.title = element_blank(),  
         legend.text = element_text(size = 20),
         axis.text = element_blank(),
         axis.title = element_blank())
 
 f1 = f1 + geom_sf(data = f1_ws, aes(color = as.factor(site)), 
-             alpha = 0, fill = "transparent", linewidth = ifelse(f1_ws$site == "C", 2, 3)) +
+             alpha = 0, fill = "transparent", linewidth = ifelse(f1_ws$site == "C", 7, 2)) +
   scale_color_manual(values = c("C" = "black", "omega1" = "grey77", "omega2" = "white", "omega3" = "firebrick"), guide = FALSE)
   
 
 
-ggsave(modify_path3("Figures/Figure1/figure1_dem.png"), f1, scale = 1.25)
+ggsave(modify_path3("Figures/Figure1/figure1_dem.png"), f1, height = 4800, width = 4800, units = "px")
 
 
 
